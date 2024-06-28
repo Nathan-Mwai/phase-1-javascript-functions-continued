ef8cf06f2197e0a6e036445e70aadb0a02eb0a73
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-06-28 12:49:51 +0300

    Updates.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    if (activity === "roller-skate"){
+        return "This Saturday, I want to roller-skate!"
+    }else{
+        return "This Saturday, I want to " + activity + "!";
+    }
+}
+
+function mondayWork(activity2 = "go to the office"){
+    if (activity2 === "go to the office"){
+        return "This Monday, I will go to the office.";
+    }else{
+        return "This Monday, I will " + activity2 + ".";
+    }
+}
+
+function wrapAdjective(adjective = "*"){
+    return function(string){
+      return `You are ${adjective}${string}${adjective}!`;
+    }
+  }
